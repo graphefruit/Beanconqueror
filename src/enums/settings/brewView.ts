b5b34da8c73e72871a5974525305467b18271c8d
Graphefruit <lars.saalbach@googlemail.com>
2017-10-26 12:42:07 +0200

    Fixed issue with enum

--- src/enums/settings/brewView.ts
+++ src/enums/settings/brewView.ts
@@ -1,5 +1,5 @@
 //LSA: IDE shows error here, but its working, so ignore it.
 export enum BREW_VIEW_ENUM {
-  SINGLE_PAGE = "SINGLE_PAGE",
-  MULTIPLE_PAGE= "MULTIPLE_PAGE",
+  SINGLE_PAGE = 0,
+  MULTIPLE_PAGE=1,
 }
