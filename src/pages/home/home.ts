b89226e2423f4b5db57281ce4d9117ac4b6fc5cc
Graphefruit <lars.saalbach@googlemail.com>
2017-10-27 09:31:37 +0200

    Home-Fix
    Fixed Hompageicon + Back on Homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,10 +7,13 @@ import {UIStatistic} from '../../services/uiStatistic';
 
 
 @Component({
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  selector:'home-page'
 })
 export class HomePage {
 
+  /**Needed app minimize for android**/
+  public isHome:boolean = true;
   public brews:number=0;
   public beans:number=0;
   public preparations:number=0;
