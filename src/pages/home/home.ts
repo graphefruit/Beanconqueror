561427f67ae61c4b6d346d87c82f389c31a78946
Graphefruit <lars.saalbach@googlemail.com>
2017-12-19 15:45:54 +0100

    Release 1.0
    Added gimmiks. Fixed issues :+1:

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 /**Services **/
-
 import {UIStatistic} from '../../services/uiStatistic';
+
+/**Third party**/
+import moment from 'moment';
+import 'moment/locale/de';
 
 
 @Component({
   templateUrl: 'home.html',
-  selector:'home-page'
+  selector: 'home-page'
 })
 export class HomePage {
 
   /**Needed app minimize for android**/
-  public isHome:boolean = true;
-  public brews:number=0;
-  public beans:number=0;
-  public preparations:number=0;
+  public isHome: boolean = true;
+  public brews: number = 0;
+  public beans: number = 0;
+  public preparations: number = 0;
+
   constructor(public navCtrl: NavController,
-              public uiStatistic:UIStatistic) {
-
-
-
+              public uiStatistic: UIStatistic) {
 
 
   }
 
+  public isChristmasTime():boolean {
+    let month: number = moment().month()+1;
+    if (month === 12)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public getGeneratedText(): string {
+
+    let year: number = moment().year();
+    let month: number = moment().month()+1;
+    let day: number = moment().day();
+    if (year === 2018) {
+      if (month == 1 && day == 1) {
+        return "Wir wünschen dir ein frohes neues Jahr 2018 und natürlich ganz viel Kaffeegenuss!";
+      }
+      else if (month == 1 && day <= 10 ) {
+        return "Wir wünschen dir (nachträglich) ein frohes neues Jahr und natürlich ganz viel Kaffeegenuss!";
+      }
+    }
+    else if (year === 2017){
+      if (month == 12 && (day == 24||day==25 || day==26)){
+        return "Wir wünschen dir fröhliche Weihnachten und ganz viel Kaffee!";
+      }
+      else if (month ==12 && day <24){
+        return "Vergiss nicht noch etwas Kaffee einzukaufen :)";
+      }
+    }
+  }
 
 }
--- src/pages/info/about/about.ts
+++ src/pages/info/about/about.ts
@@ -1,7 +1,6 @@
 /**Core**/
 import { Component } from '@angular/core';
-/**Third party**/
-import moment from 'moment';
+
 @Component({
   templateUrl: 'about.html',
 })
@@ -11,10 +10,6 @@ export class AboutPage {
   public actual_year:string;
   constructor() {
 
-  }
-
-  ionViewWillEnter() {
-    this.actual_year = String(moment().year());
   }
 
 
