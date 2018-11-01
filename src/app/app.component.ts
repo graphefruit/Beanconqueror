2111b6d1560c3e9ff0ff93d673869365ffd1ebd3
Lars Saalbach <lars.saalbach@googlemail.com>
2018-11-01 20:40:51 +0100

    Fixes #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,7 +73,9 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+
+      //#7
+      this.statusBar.show();
       this.splashScreen.hide();
 
 
