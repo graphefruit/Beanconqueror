8114a8d8611d75a5f7bb1b608177e163acf51219
Stefan Heyder <stefan.heyder@gmail.com>
2019-07-28 20:40:55 +0200

    Fix searching of brew properties

--- src/pipes/search.ts
+++ src/pipes/search.ts
@@ -2,13 +2,20 @@ import {Pipe, PipeTransform} from '@angular/core';
 
 // see this SO answer: https://stackoverflow.com/a/44764070/1629640
 @Pipe({
-  name: 'search'
+  name: 'searchBrew'
 })
 export class SearchPipe implements PipeTransform {
-  public transform(value, keys: string, term: string) {
+  public transform(brews: Array<Brew>, query: string) {
 
-    if (!term) return value;
-    return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+    if (!query) {
+      return brews;
+    }
 
+    const matchesQuery = prop => new RegExp(query, 'gi').test(prop);
+    const relevantProperties = (brew: Brew) => [brew.getBean().name, brew.getMill().name, brew.getPreparation().name];
+
+    return (brews || []).filter(
+      brew => relevantProperties(brew).some(matchesQuery)
+    )
   }
 }
