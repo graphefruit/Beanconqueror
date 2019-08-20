edf354417b80344f098e27e01631a395768010a6
Graphefruit <lars.saalbach@googlemail.com>
2019-08-20 20:29:47 +0200

    Merged #30

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {FormatDatePipe} from '../../pipes/formatDate';
 import {KeysPipe} from '../../pipes/keys';
-import {BrewAddComponent} from '../brew/brew-add/brew-add.component';
 import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 import {AsyncImageComponent} from '../../components/async-image/async-image.component';
+import {SearchPipe} from '../../pipes/search';
 
 
 @NgModule({
-  declarations: [FormatDatePipe,KeysPipe,AsyncImageComponent],
+  declarations: [FormatDatePipe, KeysPipe, AsyncImageComponent, SearchPipe],
   entryComponents: [],
   imports: [
     CommonModule,
@@ -17,7 +17,7 @@ import {AsyncImageComponent} from '../../components/async-image/async-image.comp
   ],
   providers: [
   ],
-  exports:[FormatDatePipe,KeysPipe,AsyncImageComponent],
+  exports: [FormatDatePipe, KeysPipe, AsyncImageComponent, SearchPipe],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
