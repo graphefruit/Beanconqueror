8aedf4fe50d79769ffb69a3b80ea4a19f7701fe3
Graphefruit <lars.saalbach@googlemail.com>
2020-09-24 21:57:00 +0200

    Removing obsolete code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,5 @@
 import {Component} from '@angular/core';
 import {ModalController, NavController} from '@ionic/angular';
-import {UIStatistic} from '../../services/uiStatistic';
-import moment from 'moment';
-import {BrewAddComponent} from '../brew/brew-add/brew-add.component';
 import {Router} from '@angular/router';
 import {UIBeanStorage} from '../../services/uiBeanStorage';
 import {Bean} from '../../classes/bean/bean';
@@ -20,14 +17,12 @@ export class HomePage {
 
 
   /** Needed app minimize for android */
-  public isHome: boolean = true;
   public brews: number = 0;
   public beans: number = 0;
   public preparations: number = 0;
 
   constructor (public navCtrl: NavController,
                private readonly modalCtrl: ModalController,
-               public uiStatistic: UIStatistic,
                private readonly router: Router,
                private readonly uiBeanStorage: UIBeanStorage,
                private readonly uiPreparationStorage: UIPreparationStorage,
@@ -39,12 +34,6 @@ export class HomePage {
 
   }
 
-
-  public async addBrew() {
-    const modal = await this.modalCtrl.create({component: BrewAddComponent});
-    await modal.present();
-    await modal.onWillDismiss();
-  }
 
   public showBeans() {
     this.router.navigate(['/beans']);
@@ -63,17 +52,6 @@ export class HomePage {
     this.router.navigate(['/mill']);
   }
 
-  public showSupporter() {
-    this.router.navigate(['/info/thanks']);
-  }
-  public isChristmasTime(): boolean {
-    const month: number = moment().month() + 1;
-    if (month === 12) {
-      return true;
-    }
-
-    return false;
-  }
 
   public activeBeansExists(): boolean {
     const beans: Array<Bean> = this.uiBeanStorage.getAllEntries();
