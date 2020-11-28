d0a5dd6df278f55eb7d3a7b0e189f4449b10c824
Graphefruit <lars.saalbach@googlemail.com>
2020-11-28 21:11:48 +0100

    #140 - Added cupping graph to detail

--- src/app/brew/brew-detail/brew-detail.component.ts
+++ src/app/brew/brew-detail/brew-detail.component.ts
@@ -9,6 +9,8 @@ import {Preparation} from '../../../classes/preparation/preparation';
 import {PREPARATION_STYLE_TYPE} from '../../../enums/preparations/preparationStyleTypes';
 import {UIAnalytics} from '../../../services/uiAnalytics';
 import {UIBrewHelper} from '../../../services/uiBrewHelper';
+import {TranslateService} from '@ngx-translate/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'brew-detail',
@@ -22,7 +24,7 @@ export class BrewDetailComponent implements OnInit {
   public data: Brew = new Brew();
   public settings: Settings;
 
-
+  @ViewChild('cuppingChart', {static: false}) public cuppingChart;
   private brew: IBrew;
   private loaded:boolean = false;
   constructor (private readonly modalController: ModalController,
@@ -30,7 +32,8 @@ export class BrewDetailComponent implements OnInit {
                public uiHelper: UIHelper,
                private readonly uiSettingsStorage: UISettingsStorage,
                private readonly uiAnalytics: UIAnalytics,
-               private readonly uiBrewHelper: UIBrewHelper) {
+               private readonly uiBrewHelper: UIBrewHelper,
+               private readonly translate: TranslateService) {
 
     this.settings = this.uiSettingsStorage.getSettings();
     // Moved from ionViewDidEnter, because of Ionic issues with ion-range
@@ -45,6 +48,14 @@ export class BrewDetailComponent implements OnInit {
       const copy: IBrew = this.uiHelper.copyData(this.brew);
       this.data.initializeByObject(copy);
     }
+    if (this.showCupping())
+    {
+      //Set timeout else element wont be visible
+      setTimeout( () => {
+        this.__loadCuppingChart();
+      },150);
+    }
+
     this.loaded = true;
   }
 
@@ -110,5 +121,84 @@ export class BrewDetailComponent implements OnInit {
   }
 
   public ngOnInit() {}
+
+  private showCupping() {
+    return (this.data.cupping.dry_fragrance > 0 ||
+      this.data.cupping.wet_aroma > 0 ||
+      this.data.cupping.brightness > 0 ||
+      this.data.cupping.flavor > 0 ||
+      this.data.cupping.body > 0 ||
+      this.data.cupping.finish > 0 ||
+      this.data.cupping.sweetness > 0 ||
+      this.data.cupping.clean_cup > 0 ||
+      this.data.cupping.complexity > 0 ||
+      this.data.cupping.uniformity > 0);
+  }
+
+  private __loadCuppingChart(): void {
+
+
+    const cuppingData = {
+      labels: [
+        this.translate.instant('CUPPING_SCORE_DRY_FRAGRANCE'),
+        this.translate.instant('CUPPING_SCORE_WET_AROMA'),
+        this.translate.instant('CUPPING_SCORE_BRIGHTNESS'),
+        this.translate.instant('CUPPING_SCORE_FLAVOR'),
+        this.translate.instant('CUPPING_SCORE_BODY'),
+        this.translate.instant('CUPPING_SCORE_FINISH'),
+        this.translate.instant('CUPPING_SCORE_SWEETNESS'),
+        this.translate.instant('CUPPING_SCORE_CLEAN_CUP'),
+        this.translate.instant('CUPPING_SCORE_COMPLEXITY'),
+        this.translate.instant('CUPPING_SCORE_UNIFORMITY'),
+      ],
+      datasets: [
+        {
+          fillColor: 'rgba(220,220,220,0.5)',
+          strokeColor: 'rgba(220,220,220,1)',
+          pointColor: 'rgba(220,220,220,1)',
+          pointStrokeColor: '#fff',
+          data: [
+            this.data.cupping.dry_fragrance,
+            this.data.cupping.wet_aroma,
+            this.data.cupping.brightness,
+            this.data.cupping.flavor,
+            this.data.cupping.body,
+            this.data.cupping.finish,
+            this.data.cupping.sweetness,
+            this.data.cupping.clean_cup,
+            this.data.cupping.complexity,
+            this.data.cupping.uniformity
+          ]
+        }]
+    };
+    const chartOptions = {
+      responsive: true,
+      legend: false,
+      title: {
+        display: false,
+        text: '',
+      },
+
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 10,
+          min: 0,
+          step: 0.1
+        }
+      },
+      tooltips: {
+        // Disable the on-canvas tooltip
+        enabled: false,
+      },
+      maintainAspectRatio: true,
+      aspectRatio: 1,
+    };
+    const chartObj = new Chart(this.cuppingChart.nativeElement, {
+      type: 'radar',
+      data: cuppingData,
+      options: chartOptions
+    });
+  }
 
 }
