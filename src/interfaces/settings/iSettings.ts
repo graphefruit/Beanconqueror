423ed4bd25386cea79321295c3ac0a0477fc2ad9
Graphefruit <lars.saalbach@googlemail.com>
2021-03-01 22:34:19 +0100

    Fix data versions for new updated

--- src/interfaces/settings/iSettings.ts
+++ src/interfaces/settings/iSettings.ts
@@ -12,29 +12,7 @@ export interface ISettings {
  // Properties
   brew_view: BREW_VIEW_ENUM;
   startup_view: STARTUP_VIEW_ENUM;
-  brew_temperature_time: boolean;
-  brew_time: boolean;
-  grind_size: boolean;
-  grind_weight: boolean;
-  method_of_preparation: boolean;
-  mill: boolean;
-  mill_speed: boolean;
-  mill_timer: boolean;
-  pressure_profile: boolean;
-  brew_quantity: boolean;
-  bean_type: boolean;
-  brew_temperature: boolean;
-  note: boolean;
-  coffee_type: boolean;
-  coffee_concentration: boolean;
-  coffee_first_drip_time: boolean;
-  coffee_blooming_time: boolean;
-  set_last_coffee_brew: boolean;
-  attachments: boolean;
-  rating: boolean;
-  set_custom_brew_time: boolean;
-  tds: boolean;
-  brew_beverage_quantity: boolean;
+
   language: string;
   manage_parameters: IBrewParameter;
   default_last_coffee_parameters: IBrewParameter;
