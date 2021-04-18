f5400e98e1b10d8ee9cba3abeb88997ebe281ed5
Graphefruit <lars.saalbach@googlemail.com>
2021-04-18 21:32:49 +0200

    Added first try for app link handler

--- src/services/intentHandler/intent-handler.service.ts
+++ src/services/intentHandler/intent-handler.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {UIHelper} from '../uiHelper';
+import {Deeplinks} from '@ionic-native/deeplinks/ngx';
+import {InfoComponent} from '../../app/info/info.component';
 declare var window;
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,22 @@ export class IntentHandlerService {
   public static SUPPORTED_INTENTS = {
     ADD_BEAN_ONLINE: 'ADD_BEAN_ONLINE'
   };
-  constructor(private readonly uiHelper: UIHelper) { }
+  constructor(private readonly uiHelper: UIHelper,
+              private readonly deeplinks: Deeplinks) { }
 
   public attachOnHandleOpenUrl() {
+    this.deeplinks.route( {
+      '/app/roaster/bean': InfoComponent
+    }).subscribe((match) => {
+        // match.$route - the route we matched, which is the matched entry from the arguments to route()
+        // match.$args - the args passed in the link
+        // match.$link - the full link data
+        console.log('Successfully matched route', match);
+      },
+      (nomatch) => {
+        // nomatch.$link - the full link data
+        console.error('Got a deeplink that didn\'t match', nomatch);
+      });
     (window['handleOpenURL']) = (_url) => {
       this.uiHelper.isBeanconqurorAppReady().then(async () => {
 
