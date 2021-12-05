7e14a1a96c5d81fe5a16284275531e25958b1304
Graphefruit <lars.saalbach@googlemail.com>
2021-12-05 15:32:50 +0100

    Added new prepration methods and new target sdk

--- src/classes/preparation/preparation.ts
+++ src/classes/preparation/preparation.ts
@@ -100,6 +100,20 @@ export class Preparation implements IPreparation {
         return  PREPARATION_STYLE_TYPE.ESPRESSO;
       case PREPARATION_TYPES.COLD_DRIP:
         return  PREPARATION_STYLE_TYPE.PERCOLATION;
+      case PREPARATION_TYPES.APRIL_BREWER:
+        return  PREPARATION_STYLE_TYPE.POUR_OVER;
+      case PREPARATION_TYPES.ESPRO_BLOOM:
+        return  PREPARATION_STYLE_TYPE.POUR_OVER;
+      case PREPARATION_TYPES.FELLOW_STAGG:
+        return  PREPARATION_STYLE_TYPE.POUR_OVER;
+      case PREPARATION_TYPES.HSIAO_50:
+        return  PREPARATION_STYLE_TYPE.POUR_OVER;
+      case PREPARATION_TYPES.KARLSBADER_KANNE:
+        return  PREPARATION_STYLE_TYPE.POUR_OVER;
+      case PREPARATION_TYPES.MOCCA_MASTER:
+        return  PREPARATION_STYLE_TYPE.PERCOLATION;
+      case PREPARATION_TYPES.SIPHON:
+        return  PREPARATION_STYLE_TYPE.FULL_IMMERSION;
       default:
         return PREPARATION_STYLE_TYPE.POUR_OVER;
     }
@@ -152,7 +166,20 @@ export class Preparation implements IPreparation {
         return 'beanconqueror-preparation-hand-lever';
       case PREPARATION_TYPES.COLD_DRIP:
         return 'beanconqueror-preparation-cold-drip';
-
+      case PREPARATION_TYPES.APRIL_BREWER:
+        return 'beanconqueror-preparation-april-brewer';
+      case PREPARATION_TYPES.ESPRO_BLOOM:
+        return 'beanconqueror-preparation-espro-bloom';
+      case PREPARATION_TYPES.FELLOW_STAGG:
+        return 'beanconqueror-preparation-fellow-stagg';
+      case PREPARATION_TYPES.HSIAO_50:
+        return 'beanconqueror-preparation-hsiao-50';
+      case PREPARATION_TYPES.KARLSBADER_KANNE:
+        return 'beanconqueror-preparation-karlsbader-kanne';
+      case PREPARATION_TYPES.MOCCA_MASTER:
+        return 'beanconqueror-preparation-mocca-master';
+      case PREPARATION_TYPES.SIPHON:
+        return 'beanconqueror-preparation-siphon';
       default:
         return 'beanconqueror-preparation-custom';
     }
