41f6460255e57e95efec475b95b4da1123ce14dc
Graphefruit <lars.saalbach@googlemail.com>
2022-03-14 20:11:09 +0100

    fixes

--- src/app/beans/beans.page.ts
+++ src/app/beans/beans.page.ts
@@ -447,7 +447,7 @@ export class BeansPage implements OnInit {
       },() => {});
     } else {
       // Test sample for development
-      await this.intenthandler.handleQRCodeLink('https://beanconqueror.com/?qr=f3244c61-da13-46d3-af69-f37a44976530');
+      await this.intenthandler.handleQRCodeLink('https://beanconqueror.com/?qr=Yo8JHkY3');
     }
     this.loadBeans();
     return;
