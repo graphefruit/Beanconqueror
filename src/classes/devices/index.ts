33e3f7b6cca191191dad2e45334f7eaf249af55d
Mimoja <git@mimoja.de>
2022-07-22 08:46:48 +0200

    devices: Add Eureka Precisa Scale (#393)
    * Add simplified Chiese translation. (#372)
    
    * Create zh,json
    
    * Rename zh,json to zh.json
    
    * Rename zh.json to zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update README.md
    
    * Update zh-hans.json
    
    * Update README.md
    
    * Update README.md
    
    * Update zh-hans.json
    
    * Update README.md
    
    * Update README.md
    
    * Update zh-hans.json
    
    * Create old-en.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update old-en.json
    
    * Delete old-en.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * Update zh-hans.json
    
    * update Chinese tranlation
    
    * update Chinese tranlation
    
    * change some spellings in en.json
    
    Co-authored-by: DC <34479243+DC1024@users.noreply.github.com>
    
    * devices: Add Eureka Precisa Scale
    
    The eureka precisa scale is a branded version of the Krell CFS-9002
    smart bluetooth coffee scale, sold by eureka in europe.
    
    It provies a 1-2000g range of measurement with an .1g resolution.
    The unit can be set to g, oz and ml(!).
    
    One characteristic is used for weight notifications (also containing the timer)
    One is used to write to. All write _must be_ unackknowledged writes.
    
    All logic is based on the decents de1app logic under GPLv3 here:
    https://github.com/decentespresso/de1app/commit/f935c7ef099fcac0f7298560450ad816e77012f3
    
    Testing done: None, cordova build fails on all owned systems indipendant of this change.
    
    Open Questions: how to write in an unacknowledged way?
    
    Signed-off-by: Mimoja <mimoja@mimoja.de>
    
    Co-authored-by: Graphefruit <lars.saalbach@googlemail.com>
    Co-authored-by: Jiageng Ding <jiagengding@outlook.com>
    Co-authored-by: DC <34479243+DC1024@users.noreply.github.com>

--- src/classes/devices/index.ts
+++ src/classes/devices/index.ts
@@ -5,9 +5,7 @@ import DecentScale from './decentScale';
 import FelicitaScale from './felicitaScale';
 import JimmyScale from './jimmyScale';
 import LunarScale from './lunarScale';
-import { PressureDevice } from './pressureBluetoothDevice';
-import PopsiclePressure from './popsiclePressure';
-import TransducerDirectPressure from './transducerDirectPressure';
+import EurekaPrecisaScale from './eurekaPrecisaScale';
 
 export { BluetoothScale, SCALE_TIMER_COMMAND } from './bluetoothDevice';
 export { default as DecentScale } from './decentScale';
@@ -19,18 +17,10 @@ export enum ScaleType {
   LUNAR = 'LUNAR',
   JIMMY = 'JIMMY',
   FELICITA = 'FELICITA',
+  EUREKAPRECISA = 'EUREKAPRECISA',
 }
 
-export enum PressureType {
-  POPSICLE = 'POPSICLE',
-  DIRECT = 'DIRECT',
-}
-
-export function makeDevice(
-  type: ScaleType,
-  data: PeripheralData,
-  platforms: Platforms[]
-): BluetoothScale {
+export function makeDevice(type: ScaleType, data: PeripheralData, platforms: Platforms[]): BluetoothScale {
   switch (type) {
     case ScaleType.DECENT:
       return new DecentScale(data, platforms);
@@ -40,21 +30,8 @@ export function makeDevice(
       return new JimmyScale(data, platforms);
     case ScaleType.FELICITA:
       return new FelicitaScale(data, platforms);
-    default:
-      return null;
-  }
-}
-
-export function makePressureDevice(
-  type: PressureType,
-  data: PeripheralData,
-  platforms: Platforms[]
-): PressureDevice {
-  switch (type) {
-    case PressureType.POPSICLE:
-      return new PopsiclePressure(data, platforms);
-    case PressureType.DIRECT:
-      return new TransducerDirectPressure(data, platforms);
+    case ScaleType.EUREKAPRECISA:
+      return new EurekaPrecisaScale(data, platforms);
     default:
       return null;
   }
