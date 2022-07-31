1cf90be76e316806888aecc6fdea752164ef772c
Graphefruit <lars.saalbach@googlemail.com>
2022-07-31 17:33:59 +0200

    #396 - Add wakelock when activated for edit view aswell

--- src/app/brew/brew-edit/brew-edit.component.ts
+++ src/app/brew/brew-edit/brew-edit.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
-import {UIHelper} from '../../../services/uiHelper';
-import {UIBrewStorage} from '../../../services/uiBrewStorage';
-import {IBrew} from '../../../interfaces/brew/iBrew';
-import {ModalController, NavParams, Platform} from '@ionic/angular';
-import {Brew} from '../../../classes/brew/brew';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UIHelper } from '../../../services/uiHelper';
+import { UIBrewStorage } from '../../../services/uiBrewStorage';
+import { IBrew } from '../../../interfaces/brew/iBrew';
+import { ModalController, NavParams, Platform } from '@ionic/angular';
+import { Brew } from '../../../classes/brew/brew';
 import moment from 'moment';
-import {UIToast} from '../../../services/uiToast';
-import {UIBrewHelper} from '../../../services/uiBrewHelper';
-import {BrewBrewingComponent} from '../../../components/brews/brew-brewing/brew-brewing.component';
-import {BrewTrackingService} from '../../../services/brewTracking/brew-tracking.service';
+import { UIToast } from '../../../services/uiToast';
+import { UIBrewHelper } from '../../../services/uiBrewHelper';
+import { BrewBrewingComponent } from '../../../components/brews/brew-brewing/brew-brewing.component';
+import { BrewTrackingService } from '../../../services/brewTracking/brew-tracking.service';
 import BREW_TRACKING from '../../../data/tracking/brewTracking';
-import {UIAnalytics} from '../../../services/uiAnalytics';
-
+import { UIAnalytics } from '../../../services/uiAnalytics';
+import { UISettingsStorage } from '../../../services/uiSettingsStorage';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+import { Settings } from '../../../classes/settings/settings';
 
 @Component({
   selector: 'brew-edit',
@@ -19,37 +21,59 @@ import {UIAnalytics} from '../../../services/uiAnalytics';
   styleUrls: ['./brew-edit.component.scss'],
 })
 export class BrewEditComponent implements OnInit {
-
-
   public static COMPONENT_ID: string = 'brew-edit';
-  @ViewChild('brewBrewing', {read: BrewBrewingComponent, static: false}) public brewBrewing: BrewBrewingComponent;
+  @ViewChild('brewBrewing', { read: BrewBrewingComponent, static: false })
+  public brewBrewing: BrewBrewingComponent;
   public data: Brew = new Brew();
+  public settings: Settings;
 
-
-  constructor (private readonly modalController: ModalController,
-               private readonly navParams: NavParams,
-               private readonly uiBrewStorage: UIBrewStorage,
-               private readonly uiHelper: UIHelper,
-               private readonly uiToast: UIToast,
-               private readonly platform: Platform,
-               private readonly uiBrewHelper: UIBrewHelper,
-               private readonly brewTracking: BrewTrackingService,
-               private readonly uiAnalytics: UIAnalytics) {
-
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly navParams: NavParams,
+    private readonly uiBrewStorage: UIBrewStorage,
+    private readonly uiHelper: UIHelper,
+    private readonly uiToast: UIToast,
+    private readonly platform: Platform,
+    private readonly uiBrewHelper: UIBrewHelper,
+    private readonly brewTracking: BrewTrackingService,
+    private readonly uiAnalytics: UIAnalytics,
+    private readonly uiSettingsStorage: UISettingsStorage,
+    private readonly insomnia: Insomnia
+  ) {
+    this.settings = this.uiSettingsStorage.getSettings();
     // Moved from ionViewDidEnter, because of Ionic issues with ion-range
     const brew: IBrew = this.uiHelper.copyData(this.navParams.get('brew'));
 
     if (brew !== undefined) {
       this.data.initializeByObject(brew);
     }
-
   }
 
+  public ionViewDidEnter(): void {
+    if (this.settings.wake_lock) {
+      this.insomnia.keepAwake().then(
+        () => {},
+        () => {}
+      );
+    }
+  }
+  public ionViewWillLeave() {
+    if (this.settings.wake_lock) {
+      this.insomnia.allowSleepAgain().then(
+        () => {},
+        () => {}
+      );
+    }
+  }
 
   public dismiss(): void {
-    this.modalController.dismiss({
-      dismissed: true
-    },undefined,BrewEditComponent.COMPONENT_ID);
+    this.modalController.dismiss(
+      {
+        dismissed: true,
+      },
+      undefined,
+      BrewEditComponent.COMPONENT_ID
+    );
   }
 
   public async updateBrew() {
@@ -65,18 +89,20 @@ export class BrewEditComponent implements OnInit {
     }
     await this.uiBrewStorage.update(this.data);
 
-
-
     this.brewTracking.trackBrew(this.data);
 
     this.uiToast.showInfoToast('TOAST_BREW_EDITED_SUCCESSFULLY');
-    this.uiAnalytics.trackEvent(BREW_TRACKING.TITLE, BREW_TRACKING.ACTIONS.EDIT_FINISH);
+    this.uiAnalytics.trackEvent(
+      BREW_TRACKING.TITLE,
+      BREW_TRACKING.ACTIONS.EDIT_FINISH
+    );
     this.dismiss();
   }
 
   public ngOnInit() {
-    this.uiAnalytics.trackEvent(BREW_TRACKING.TITLE, BREW_TRACKING.ACTIONS.EDIT);
+    this.uiAnalytics.trackEvent(
+      BREW_TRACKING.TITLE,
+      BREW_TRACKING.ACTIONS.EDIT
+    );
   }
-
-
 }
