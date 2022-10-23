52883d8a69d30f7b05149ad1e2dd49f599300a05
Graphefruit <lars.saalbach@googlemail.com>
2022-10-23 16:04:54 +0200

    Fix filter to fixing decimal numbers

--- src/app/brew/brew-filter/brew-filter.component.ts
+++ src/app/brew/brew-filter/brew-filter.component.ts
@@ -1,19 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {ModalController, NavParams} from '@ionic/angular';
-import {UIBrewHelper} from '../../../services/uiBrewHelper';
-import {UIHelper} from '../../../services/uiHelper';
-import {UISettingsStorage} from '../../../services/uiSettingsStorage';
-import {IBrewPageFilter} from '../../../interfaces/brew/iBrewPageFilter';
-import {UIPreparationStorage} from '../../../services/uiPreparationStorage';
-import {UIMillStorage} from '../../../services/uiMillStorage';
-import {UIBeanStorage} from '../../../services/uiBeanStorage';
-import {Bean} from '../../../classes/bean/bean';
-import {Mill} from '../../../classes/mill/mill';
-import {Preparation} from '../../../classes/preparation/preparation';
-import {Settings} from '../../../classes/settings/settings';
-import {PreparationTool} from '../../../classes/preparation/preparationTool';
-import {UIBrewStorage} from '../../../services/uiBrewStorage';
-import {Brew} from '../../../classes/brew/brew';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { UIBrewHelper } from '../../../services/uiBrewHelper';
+import { UIHelper } from '../../../services/uiHelper';
+import { UISettingsStorage } from '../../../services/uiSettingsStorage';
+import { IBrewPageFilter } from '../../../interfaces/brew/iBrewPageFilter';
+import { UIPreparationStorage } from '../../../services/uiPreparationStorage';
+import { UIMillStorage } from '../../../services/uiMillStorage';
+import { UIBeanStorage } from '../../../services/uiBeanStorage';
+import { Bean } from '../../../classes/bean/bean';
+import { Mill } from '../../../classes/mill/mill';
+import { Preparation } from '../../../classes/preparation/preparation';
+import { Settings } from '../../../classes/settings/settings';
+import { PreparationTool } from '../../../classes/preparation/preparationTool';
+import { UIBrewStorage } from '../../../services/uiBrewStorage';
+import { Brew } from '../../../classes/brew/brew';
 
 @Component({
   selector: 'brew-filter',
@@ -30,22 +30,22 @@ export class BrewFilterComponent implements OnInit {
   public mills: Array<Mill> = [];
   public segment: string = 'open';
 
-  constructor(private readonly modalController: ModalController,
-              private readonly uiBrewHelper: UIBrewHelper,
-              private readonly navParams: NavParams,
-              private readonly uiHelper: UIHelper,
-              private readonly uiSettingsStorage: UISettingsStorage,
-              private readonly uiPreparationStorage: UIPreparationStorage,
-              private readonly uiBeanStorage: UIBeanStorage,
-              private readonly uiMillStorage: UIMillStorage,
-              private readonly uiBrewStorage: UIBrewStorage) {
-
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly uiBrewHelper: UIBrewHelper,
+    private readonly navParams: NavParams,
+    public readonly uiHelper: UIHelper,
+    private readonly uiSettingsStorage: UISettingsStorage,
+    private readonly uiPreparationStorage: UIPreparationStorage,
+    private readonly uiBeanStorage: UIBeanStorage,
+    private readonly uiMillStorage: UIMillStorage,
+    private readonly uiBrewStorage: UIBrewStorage
+  ) {
     this.settings = this.uiSettingsStorage.getSettings();
     this.filter = this.settings.GET_BREW_FILTER();
   }
 
   public ngOnInit() {
-
     this.segment = this.navParams.get('segment');
     this.filter = this.uiHelper.copyData(this.navParams.get('brew_filter'));
     this.__reloadFilterSettings();
@@ -55,25 +55,31 @@ export class BrewFilterComponent implements OnInit {
     const isOpen = this.segment === 'open';
     let brewsFiltered: Array<Brew> = [];
     if (isOpen) {
-      brewsFiltered =  this.uiBrewStorage.getAllEntries().filter((e) =>
-          e.getBean().finished === !isOpen &&
-          e.getMill().finished === !isOpen &&
-          e.getPreparation().finished === !isOpen
+      brewsFiltered = this.uiBrewStorage
+        .getAllEntries()
+        .filter(
+          (e) =>
+            e.getBean().finished === !isOpen &&
+            e.getMill().finished === !isOpen &&
+            e.getPreparation().finished === !isOpen
         );
-      }
-    else {
-      brewsFiltered =  this.uiBrewStorage.getAllEntries().filter((e) =>
-        e.getBean().finished === !isOpen ||
-        e.getMill().finished === !isOpen ||
-        e.getPreparation().finished === !isOpen
-      );
+    } else {
+      brewsFiltered = this.uiBrewStorage
+        .getAllEntries()
+        .filter(
+          (e) =>
+            e.getBean().finished === !isOpen ||
+            e.getMill().finished === !isOpen ||
+            e.getPreparation().finished === !isOpen
+        );
     }
     let maxBrewRating = maxSettingsRating;
     if (brewsFiltered.length > 0) {
-      const maxRating = brewsFiltered.reduce((p, c) => p.rating > c.rating ? p : c);
+      const maxRating = brewsFiltered.reduce((p, c) =>
+        p.rating > c.rating ? p : c
+      );
       maxBrewRating = maxRating.rating;
     }
-
 
     if (maxBrewRating > maxSettingsRating) {
       return maxBrewRating;
@@ -83,12 +89,10 @@ export class BrewFilterComponent implements OnInit {
 
   public hasPreparationTools() {
     for (const uuid of this.filter.method_of_preparation) {
-
       const preparation = this.uiPreparationStorage.getByUUID(uuid);
       if (preparation.tools.length > 0) {
         return true;
       }
-
     }
     return false;
   }
@@ -98,56 +102,62 @@ export class BrewFilterComponent implements OnInit {
   }
 
   public getPreparationTools() {
-    const preparationTools: { name: string, tool: PreparationTool }[] = [];
+    const preparationTools: { name: string; tool: PreparationTool }[] = [];
     for (const uuid of this.filter.method_of_preparation) {
-
-      const preparation: Preparation = this.uiPreparationStorage.getByUUID(uuid);
+      const preparation: Preparation =
+        this.uiPreparationStorage.getByUUID(uuid);
       if (preparation.tools.length > 0) {
         for (const tool of preparation.tools) {
           preparationTools.push({
-            name:preparation.name,
-            tool: tool
+            name: preparation.name,
+            tool: tool,
           });
         }
-
       }
-
     }
     return preparationTools;
   }
 
   public dismiss(): void {
     this.modalController.dismiss({
-      brew_filter: undefined
+      brew_filter: undefined,
     });
   }
 
   public useFilter() {
     this.modalController.dismiss({
-      brew_filter: this.uiHelper.copyData(this.filter)
+      brew_filter: this.uiHelper.copyData(this.filter),
     });
   }
 
   public resetFilter() {
     this.filter = this.settings.GET_BREW_FILTER();
-    this.modalController.dismiss({
-      brew_filter: this.uiHelper.copyData(this.filter)
-    },undefined,BrewFilterComponent.COMPONENT_ID);
+    this.modalController.dismiss(
+      {
+        brew_filter: this.uiHelper.copyData(this.filter),
+      },
+      undefined,
+      BrewFilterComponent.COMPONENT_ID
+    );
   }
 
-
   private __reloadFilterSettings() {
-    this.method_of_preparations = this.uiPreparationStorage.getAllEntries()
+    this.method_of_preparations = this.uiPreparationStorage
+      .getAllEntries()
       .sort((a, b) => a.name.localeCompare(b.name));
-    this.beans = this.uiBeanStorage.getAllEntries()
+    this.beans = this.uiBeanStorage
+      .getAllEntries()
       .sort((a, b) => a.name.localeCompare(b.name));
-    this.mills = this.uiMillStorage.getAllEntries()
+    this.mills = this.uiMillStorage
+      .getAllEntries()
       .sort((a, b) => a.name.localeCompare(b.name));
 
     if (this.segment === 'open') {
       this.beans = this.beans.filter((e) => e.finished === false);
       this.mills = this.mills.filter((e) => e.finished === false);
-      this.method_of_preparations = this.method_of_preparations.filter((e) => e.finished === false);
+      this.method_of_preparations = this.method_of_preparations.filter(
+        (e) => e.finished === false
+      );
     } else {
       this.beans = this.beans.filter((e) => e.finished === true);
     }
