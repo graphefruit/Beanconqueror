264191e255d10212dd94ce6d46ca86b2d3beb4b9
Graphefruit <lars.saalbach@googlemail.com>
2023-02-28 20:29:42 +0100

    #501 - Resetting filter, anyway, because we got a roast range

--- src/services/uiUpdate.ts
+++ src/services/uiUpdate.ts
@@ -491,10 +491,10 @@ export class UIUpdate {
               );
 
               settings_v9.bean_manage_parameters.activateAll();
-              // Reset filter, because we got a new sort on beans
-              settings_v9.resetFilter();
-              await this.uiSettingsStorage.saveSettings(settings_v9);
             }
+            // Reset filter, because we got a new sort on beans
+            settings_v9.resetFilter();
+            await this.uiSettingsStorage.saveSettings(settings_v9);
             const preparationListV9: Array<Preparation> =
               this.uiPreparationStorage.getAllEntries();
             for (const prep of preparationListV9) {
