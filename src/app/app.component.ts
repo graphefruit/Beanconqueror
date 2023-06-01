3122e77e4be415cf30a6fe6d89496aa6a9e2c53d
Lars Saalbach <lars.saalbach@googlemail.com>
2023-06-01 23:40:09 +0200

    Going to 15 sec finding devices instead of 1 minute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -719,7 +719,7 @@ export class AppComponent implements AfterViewInit {
     try {
       if (searchIds.length > 0) {
         //Just search if we raly got id's
-        this.bleManager.findDeviceWithDirectIds(searchIds, 60000);
+        this.bleManager.findDeviceWithDirectIds(searchIds, 15000);
       }
     } catch (ex) {}
   }
