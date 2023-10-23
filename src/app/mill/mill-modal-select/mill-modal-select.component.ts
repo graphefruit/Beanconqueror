8c356f71ebbb22040a77c0a5aa467a745b3d43b4
Lars Saalbach <lars.saalbach@googlemail.com>
2023-10-23 20:07:32 +0200

    #616, #615, set last used and last grind size

--- src/app/mill/mill-modal-select/mill-modal-select.component.ts
+++ src/app/mill/mill-modal-select/mill-modal-select.component.ts
@@ -1,7 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ModalController} from '@ionic/angular';
-import {UIMillStorage} from '../../../services/uiMillStorage';
-import {Mill} from '../../../classes/mill/mill';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { UIMillStorage } from '../../../services/uiMillStorage';
+import { Mill } from '../../../classes/mill/mill';
+import { Brew } from '../../../classes/brew/brew';
+import { UIBrewHelper } from '../../../services/uiBrewHelper';
+import { UIMillHelper } from '../../../services/uiMillHelper';
 
 @Component({
   selector: 'mill-modal-select',
@@ -17,18 +20,18 @@ export class MillModalSelectComponent implements OnInit {
   @Input() public multiple: boolean;
   @Input() private selectedValues: Array<string>;
   @Input() public showFinished: boolean;
-  constructor(private readonly modalController: ModalController,
-              private readonly uiMillStorage: UIMillStorage) {
-
-
-  }
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly uiMillStorage: UIMillStorage,
+    private readonly uiMillHelper: UIMillHelper
+  ) {}
 
   public ionViewDidEnter(): void {
     this.objs = this.uiMillStorage.getAllEntries();
     if (this.multiple) {
       for (const obj of this.objs) {
-
-        this.multipleSelection[obj.config.uuid] = this.selectedValues.filter((e) => e === obj.config.uuid).length > 0;
+        this.multipleSelection[obj.config.uuid] =
+          this.selectedValues.filter((e) => e === obj.config.uuid).length > 0;
       }
     } else {
       if (this.selectedValues.length > 0) {
@@ -37,13 +40,12 @@ export class MillModalSelectComponent implements OnInit {
     }
   }
 
-  public ngOnInit() {
-
-  }
+  public ngOnInit() {}
 
   public getOpenMills(): Array<Mill> {
-    return this.objs.filter(
-      (e) => !e.finished).sort( (a,b) => {
+    return this.objs
+      .filter((e) => !e.finished)
+      .sort((a, b) => {
         const nameA = a.name.toUpperCase();
         const nameB = b.name.toUpperCase();
 
@@ -55,13 +57,13 @@ export class MillModalSelectComponent implements OnInit {
         }
 
         return 0;
-      }
-    );
+      });
   }
 
   public getArchivedMills(): Array<Mill> {
-    return this.objs.filter(
-      (e) => e.finished).sort( (a,b) => {
+    return this.objs
+      .filter((e) => e.finished)
+      .sort((a, b) => {
         const nameA = a.name.toUpperCase();
         const nameB = b.name.toUpperCase();
 
@@ -73,16 +75,12 @@ export class MillModalSelectComponent implements OnInit {
         }
 
         return 0;
-      }
-    );
+      });
   }
-
 
   public async choose(): Promise<void> {
     const chosenKeys: Array<string> = [];
     if (this.multiple) {
-
-
       for (const key in this.multipleSelection) {
         if (this.multipleSelection[key] === true) {
           chosenKeys.push(key);
@@ -99,14 +97,50 @@ export class MillModalSelectComponent implements OnInit {
     }
 
     selected_text = selected_text.substr(0, selected_text.lastIndexOf(', '));
-    this.modalController.dismiss({
-      selected_values: chosenKeys,
-      selected_text: selected_text,
-    },undefined, MillModalSelectComponent.COMPONENT_ID);
+    this.modalController.dismiss(
+      {
+        selected_values: chosenKeys,
+        selected_text: selected_text,
+      },
+      undefined,
+      MillModalSelectComponent.COMPONENT_ID
+    );
   }
 
   public async dismiss(): Promise<void> {
-    this.modalController.dismiss(undefined,undefined,MillModalSelectComponent.COMPONENT_ID);
+    this.modalController.dismiss(
+      undefined,
+      undefined,
+      MillModalSelectComponent.COMPONENT_ID
+    );
   }
 
+  public getLastUsedGrindSizeForBrew(_mill: Mill): string {
+    let relatedBrews: Array<Brew> = this.uiMillHelper.getAllBrewsForThisMill(
+      _mill.config.uuid
+    );
+    if (relatedBrews.length > 0) {
+      relatedBrews = UIBrewHelper.sortBrews(relatedBrews);
+      return relatedBrews[0].grind_size;
+    }
+    return '-';
+  }
+
+  public lastUsed(_mill: Mill): number {
+    let relatedBrews: Array<Brew> = this.uiMillHelper.getAllBrewsForThisMill(
+      _mill.config.uuid
+    );
+    if (relatedBrews.length > 0) {
+      relatedBrews = UIBrewHelper.sortBrews(relatedBrews);
+      return relatedBrews[0].config.unix_timestamp;
+    }
+    return -1;
+  }
+
+  public getBrewsCount(_mill: Mill): number {
+    const relatedBrews: Array<Brew> = this.uiMillHelper.getAllBrewsForThisMill(
+      _mill.config.uuid
+    );
+    return relatedBrews.length;
+  }
 }
