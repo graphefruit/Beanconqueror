ed54db5a9a317375f1b6b24b0be3aef2681d3a4f
Lars Saalbach <lars.saalbach@googlemail.com>
2023-11-20 20:54:49 +0100

    Adding the first update messages for 7.0.0

--- src/services/uiUpdate.ts
+++ src/services/uiUpdate.ts
@@ -578,7 +578,7 @@ export class UIUpdate {
         versionCode = await this.appVersion.getVersionNumber();
       } else {
         // Hardcored for testing
-        versionCode = '6.6.2';
+        versionCode = '7.0.0';
       }
       const version: Version = this.uiVersionStorage.getVersion();
       const displayingVersions =
