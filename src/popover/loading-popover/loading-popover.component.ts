792823cead236341be9e844d0e6bac1bda17312b
Lars Saalbach <lars.saalbach@googlemail.com>
2024-03-21 21:04:34 +0100

    Added logs to the loading popover

--- src/popover/loading-popover/loading-popover.component.ts
+++ src/popover/loading-popover/loading-popover.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { LogTextComponent } from '../../app/info/log/log-text/log-text.component';
+import { UILog } from '../../services/uiLog';
+import { ShareService } from '../../services/shareService/share-service.service';
 
 @Component({
   selector: 'loading-popover',
@@ -12,7 +15,11 @@ export class LoadingPopoverComponent implements OnInit {
   public showDismissAfterSpecificTimeout: boolean;
   @Input('message') public message: string;
   private timeoutFunc = null;
-  constructor(private readonly modalController: ModalController) {}
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly uiLog: UILog,
+    private shareService: ShareService
+  ) {}
 
   public ngOnInit() {
     if (this.showDismissAfterSpecificTimeout) {
@@ -31,6 +38,27 @@ export class LoadingPopoverComponent implements OnInit {
     this.modalController.dismiss({
       dismissed: true,
     });
+  }
+
+  public async sendLogs() {
+    const logs = this.uiLog.getLogs();
+    const stringifiedJSON = JSON.stringify(logs);
+    const blob = new Blob([stringifiedJSON], {
+      type: 'application/json',
+    });
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64data = reader.result as string;
+      this.shareService.shareFile('Beanconqueror_logs', base64data);
+    };
+    reader.onerror = async () => {
+      const modal = await this.modalController.create({
+        component: LogTextComponent,
+      });
+      await modal.present();
+      await modal.onWillDismiss();
+    };
   }
   public showDismissButton() {
     this.__showDismissButton = true;
