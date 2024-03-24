02c4c29d45e2c538aa0a95e27d9de0803f5ab037
Adrian John <adrianjohn6543@gmail.com>
2024-03-24 18:50:17 +0100

    fix: beans-add.component.spec.ts

--- src/app/beans/beans-add/beans-add.component.spec.ts
+++ src/app/beans/beans-add/beans-add.component.spec.ts
@@ -6,8 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { KeysPipe } from '../../../pipes/keys';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, ModalController, NavParams } from '@ionic/angular';
-import { Storage } from '@ionic/storage';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { File } from '@awesome-cordova-plugins/file/ngx';
 import { Camera } from '@awesome-cordova-plugins/camera/ngx';
@@ -15,17 +14,28 @@ import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
 import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavParamsMock } from '../../../classes/mock/NavParamsMock';
+import { UIBeanStorage } from '../../../services/uiBeanStorage';
+import { UIHelper } from '../../../services/uiHelper';
+import { UIFileHelper } from '../../../services/uiFileHelper';
+import { UIAnalytics } from '../../../services/uiAnalytics';
+import { UIBeanHelper } from '../../../services/uiBeanHelper';
+import { UISettingsStorage } from '../../../services/uiSettingsStorage';
+import { Settings } from '../../../classes/settings/settings';
 
 describe('BeansAddComponent', () => {
   let component: BeansAddComponent;
   let fixture: ComponentFixture<BeansAddComponent>;
+
   beforeEach(waitForAsync(() => {
+    const mockedSettings = {
+      bean_manage_parameters: {
+        bean_information: true,
+      },
+    } as Settings;
     TestBed.configureTestingModule({
       imports: [
         TranslateModule.forRoot(),
         FormsModule,
-        Storage,
         CommonModule,
         IonicModule,
       ],
@@ -33,24 +43,39 @@ describe('BeansAddComponent', () => {
       providers: [
         { provide: InAppBrowser },
         { provide: ModalController },
-        { provide: NavParams, useClass: NavParamsMock },
-        { provide: Storage },
+        { provide: UIBeanStorage, useValue: {} },
         { provide: File },
         { provide: Camera },
         { provide: ImagePicker },
         { provide: AndroidPermissions },
-
         { provide: Router },
+        { provide: UIHelper, useValue: {} },
+        { provide: UIFileHelper, useValue: {} },
+        { provide: UIAnalytics, useValue: {} },
+        {
+          provide: UIBeanHelper,
+          useValue: {
+            fieldVisible(_settingsField: boolean, _data?: any): boolean {
+              return _settingsField;
+            },
+          } as UIBeanHelper,
+        },
+        {
+          provide: UISettingsStorage,
+          useValue: {
+            getSettings(): Settings {
+              return mockedSettings;
+            },
+          },
+        },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(BeansAddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
