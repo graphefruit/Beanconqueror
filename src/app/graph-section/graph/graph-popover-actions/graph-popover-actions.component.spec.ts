106701080d7c71f27ac79c3293c109bd2d224c0f
Adrian John <adrianjohn6543@gmail.com>
2024-03-24 15:39:58 +0100

    fix: graph-popover-actions.component.spec.ts

--- src/app/graph-section/graph/graph-popover-actions/graph-popover-actions.component.spec.ts
+++ src/app/graph-section/graph/graph-popover-actions/graph-popover-actions.component.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { GraphPopoverActionsComponent } from './graph-popover-actions.component';
+import {
+  DefaultLangChangeEvent,
+  LangChangeEvent,
+  TranslatePipe,
+  TranslateService,
+  TranslationChangeEvent,
+} from '@ngx-translate/core';
+import { NEVER, Observable } from 'rxjs';
+import { EventEmitter } from '@angular/core';
 
 describe('GraphPopoverActionsComponent', () => {
   let component: GraphPopoverActionsComponent;
@@ -9,8 +18,36 @@ describe('GraphPopoverActionsComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [GraphPopoverActionsComponent],
+      declarations: [GraphPopoverActionsComponent, TranslatePipe],
       imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: TranslateService,
+          useValue: {
+            instant: (
+              _key: string | Array<string>,
+              _interpolateParams?: any
+            ) => {
+              return 'hello';
+            },
+            get(
+              _key: string | Array<string>,
+              _interpolateParams?: any
+            ): Observable<any> {
+              return NEVER;
+            },
+            get onTranslationChange(): EventEmitter<TranslationChangeEvent> {
+              return new EventEmitter();
+            },
+            get onLangChange(): EventEmitter<LangChangeEvent> {
+              return new EventEmitter<LangChangeEvent>();
+            },
+            get onDefaultLangChange(): EventEmitter<DefaultLangChangeEvent> {
+              return new EventEmitter();
+            },
+          } as TranslateService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GraphPopoverActionsComponent);
