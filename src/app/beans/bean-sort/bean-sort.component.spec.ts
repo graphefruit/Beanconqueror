ae7d9716770e0791930433043d71c8ded991c6b5
Adrian John <adrianjohn6543@gmail.com>
2024-03-24 17:50:35 +0100

    fix: bean-sort.component.spec.ts

--- src/app/beans/bean-sort/bean-sort.component.spec.ts
+++ src/app/beans/bean-sort/bean-sort.component.spec.ts
@@ -2,7 +2,11 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule, NavParams } from '@ionic/angular';
 
 import { BeanSortComponent } from './bean-sort.component';
-import { NavParamsMock } from '../../../classes/mock';
+import { NavParamsMock, TranslateServiceMock } from '../../../classes/mock';
+import { UIHelper } from '../../../services/uiHelper';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { BEAN_SORT_ORDER } from '../../../enums/beans/beanSortOrder';
+import { BEAN_SORT_AFTER } from '../../../enums/beans/beanSortAfter';
 
 describe('BeanSortComponent', () => {
   let component: BeanSortComponent;
@@ -10,16 +14,31 @@ describe('BeanSortComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [BeanSortComponent],
+      declarations: [BeanSortComponent, TranslatePipe],
       imports: [IonicModule.forRoot()],
       providers: [
         {
           provide: NavParams,
           useClass: NavParamsMock,
         },
+        {
+          provide: UIHelper,
+          useValue: {
+            copyData(_value: any): any {
+              return _value;
+            },
+          },
+        },
+        {
+          provide: TranslateService,
+          useValue: TranslateServiceMock,
+        },
       ],
     }).compileComponents();
-
+    NavParamsMock.setParams({
+      sort_order: BEAN_SORT_ORDER.UNKOWN,
+      sort_after: BEAN_SORT_AFTER.UNKOWN,
+    });
     fixture = TestBed.createComponent(BeanSortComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
