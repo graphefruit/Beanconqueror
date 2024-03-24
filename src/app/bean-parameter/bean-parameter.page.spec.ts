8aa49b4dad456a9568ee4adcbdb6da512d93d616
Adrian John <adrianjohn6543@gmail.com>
2024-03-24 16:18:56 +0100

    fix: bean-parameter.page.spec.ts

--- src/app/bean-parameter/bean-parameter.page.spec.ts
+++ src/app/bean-parameter/bean-parameter.page.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { BeanParameterPage } from './bean-parameter.page';
+import {
+  DefaultLangChangeEvent,
+  LangChangeEvent,
+  TranslatePipe,
+  TranslateService,
+  TranslationChangeEvent,
+} from '@ngx-translate/core';
+import { NEVER, Observable } from 'rxjs';
+import { EventEmitter } from '@angular/core';
 
 describe('BeanParameterPage', () => {
   let component: BeanParameterPage;
@@ -9,8 +18,36 @@ describe('BeanParameterPage', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [BeanParameterPage],
+      declarations: [BeanParameterPage, TranslatePipe],
       imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: TranslateService,
+          useValue: {
+            instant: (
+              _key: string | Array<string>,
+              _interpolateParams?: any
+            ) => {
+              return 'hello';
+            },
+            get(
+              _key: string | Array<string>,
+              _interpolateParams?: any
+            ): Observable<any> {
+              return NEVER;
+            },
+            get onTranslationChange(): EventEmitter<TranslationChangeEvent> {
+              return new EventEmitter();
+            },
+            get onLangChange(): EventEmitter<LangChangeEvent> {
+              return new EventEmitter<LangChangeEvent>();
+            },
+            get onDefaultLangChange(): EventEmitter<DefaultLangChangeEvent> {
+              return new EventEmitter();
+            },
+          } as TranslateService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BeanParameterPage);
