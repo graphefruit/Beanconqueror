ebeae8750e8d8c7d17f612430057e75c61f2dd3d
Adrian John <adrianjohn6543@gmail.com>
2024-03-24 22:07:30 +0100

    fix: graph-section.page.spec.ts

--- src/app/graph-section/graph-section.page.spec.ts
+++ src/app/graph-section/graph-section.page.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GraphSectionPage } from './graph-section.page';
+import { InfoComponent } from '../info/info.component';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('GraphSectionPage', () => {
   let component: GraphSectionPage;
   let fixture: ComponentFixture<GraphSectionPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GraphSectionPage],
+      imports: [TranslateModule.forRoot()],
+    }).compileComponents();
     fixture = TestBed.createComponent(GraphSectionPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
