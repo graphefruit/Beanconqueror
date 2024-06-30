055cf3eef577a2f3d6a905e2527e8621cbbb9149
Juan Silveira <jjsilo@gmail.com>
2024-06-30 13:23:07 +0900

    Uncomment and partially fix BrewMaximizeControls.
    * Split beforeEach in async and sync for compileComponents.
    * Move this.settings assignment to constructor to avoid
      ExpressionChangedAfterItHasBeenCheckedError.
    * Mark the test as pending because there's a deeper error, this.brew
      is references in the template but the field is missing in the
      Component.

--- src/app/brew/brew-maximize-controls/brew-maximize-controls.component.spec.ts
+++ src/app/brew/brew-maximize-controls/brew-maximize-controls.component.spec.ts
@@ -1,46 +1,50 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { IonicModule } from '@ionic/angular';
-//
-// import { BrewMaximizeControlsComponent } from './brew-maximize-controls.component';
-// import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
-// import { UIHelper } from '../../../services/uiHelper';
-// import { UIHelperMock } from '../../../classes/mock';
-// import { Storage } from '@ionic/storage';
-// import { TranslateModule } from '@ngx-translate/core';
-// import { BrewBrewingComponent } from '../../../components/brews/brew-brewing/brew-brewing.component';
-// import { Preparation } from '../../../classes/preparation/preparation';
-//
-// describe('BrewMaximizeControlsComponent', () => {
-//   let component: BrewMaximizeControlsComponent;
-//   let fixture: ComponentFixture<BrewMaximizeControlsComponent>;
-//
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [BrewMaximizeControlsComponent],
-//       imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
-//       providers: [{ provide: ScreenOrientation }, { provide: UIHelper, useClass: UIHelperMock }, { provide: Storage }]
-//     }).compileComponents();
-//
-//     fixture = TestBed.createComponent(BrewMaximizeControlsComponent);
-//     component = fixture.componentInstance;
-//     component.brewComponent = {
-//       timer: {
-//         timer: {
-//           runTimer: false,
-//         },
-//         getSeconds(): number {
-//           return 10;
-//         },
-//       },
-//       getPreparation(): Preparation {
-//         return new Preparation();
-//       }
-//     } as BrewBrewingComponent;
-//     fixture.detectChanges();
-//   }));
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-// Fixme ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value:
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { BrewMaximizeControlsComponent } from './brew-maximize-controls.component';
+import { UIHelper } from '../../../services/uiHelper';
+import { UIHelperMock } from '../../../classes/mock';
+import { Storage } from '@ionic/storage';
+import { BrewBrewingComponent } from '../../../components/brews/brew-brewing/brew-brewing.component';
+import { Preparation } from '../../../classes/preparation/preparation';
+import { TranslateModule } from '@ngx-translate/core';
+
+describe('BrewMaximizeControlsComponent', () => {
+  let component: BrewMaximizeControlsComponent;
+  let fixture: ComponentFixture<BrewMaximizeControlsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [BrewMaximizeControlsComponent],
+      imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+      providers: [
+        { provide: UIHelper, useClass: UIHelperMock },
+        { provide: Storage },
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BrewMaximizeControlsComponent);
+    component = fixture.componentInstance;
+    component.brewComponent = {
+      timer: {
+        timer: {
+          runTimer: false,
+        },
+        getSeconds(): number {
+          return 10;
+        },
+      },
+      getPreparation(): Preparation {
+        return new Preparation();
+      },
+    } as BrewBrewingComponent;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    pending(); // FIXME: Cannot read properties of undefined (reading 'brew_time')
+    expect(component).toBeTruthy();
+  });
+});
