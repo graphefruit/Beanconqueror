a51ac4b00eb37e02a530441295b5bf38c7039697
Juan Silveira <jjsilo@gmail.com>
2024-07-01 05:09:13 +0900

    Use MockBrew in BrewBrewingGraphComponent test.
    Redo from 76e0f16 lost in merge.

--- src/components/brews/brew-brewing-graph/brew-brewing-graph.component.spec.ts
+++ src/components/brews/brew-brewing-graph/brew-brewing-graph.component.spec.ts
@@ -5,13 +5,11 @@ import { BrewBrewingGraphComponent } from './brew-brewing-graph.component';
 import { Storage } from '@ionic/storage';
 import { TranslateModule } from '@ngx-translate/core';
 import { UIHelper } from '../../../services/uiHelper';
-import { UIHelperMock } from '../../../classes/mock';
+import { BrewMock, UIHelperMock } from '../../../classes/mock';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { File } from '@awesome-cordova-plugins/file/ngx';
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 import { FileTransfer } from '@awesome-cordova-plugins/file-transfer/ngx';
-import { Brew } from '../../../classes/brew/brew';
-import { Preparation } from '../../../classes/preparation/preparation';
 
 describe('BrewBrewingGraphComponent', () => {
   let component: BrewBrewingGraphComponent;
@@ -39,7 +37,7 @@ describe('BrewBrewingGraphComponent', () => {
     fixture = TestBed.createComponent(BrewBrewingGraphComponent);
     component = fixture.componentInstance;
 
-    component.data = new MockBrew();
+    component.data = new BrewMock();
     fixture.detectChanges();
   });
 
@@ -47,11 +45,3 @@ describe('BrewBrewingGraphComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
-class MockBrew extends Brew {
-  public _preparation: Preparation = new Preparation();
-
-  public getPreparation(): Preparation {
-    return this._preparation;
-  }
-}
