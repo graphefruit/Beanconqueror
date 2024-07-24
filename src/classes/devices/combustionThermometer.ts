c3bec59558fe8a5a54295882c761793e7d05ca68
Lars Saalbach <lars.saalbach@googlemail.com>
2024-07-24 12:52:18 +0200

    #754 - Big thanks to Mimoja (https://github.com/Mimoja) which made all the byte/bit shifts to get the real temperature to make it all working!

--- src/classes/devices/combustionThermometer.ts
+++ src/classes/devices/combustionThermometer.ts
@@ -12,7 +12,7 @@ export class CombustionThermometer extends TemperatureDevice {
   public static TEMPERATURE_CHAR_UUID = '00000101-CAAB-3792-3D44-97AE51C1407A';
 
   private logger: Logger;
-
+  public bitsPerTemperature = 13;
   constructor(data: PeripheralData) {
     super(data);
     this.connect();
@@ -44,7 +44,9 @@ export class CombustionThermometer extends TemperatureDevice {
 
       async (_data: any) => {
         console.log(_data);
-        this.parseStatusUpdate(new Uint8Array(_data));
+        const data = new Uint8Array(_data);
+
+        this.parseStatusUpdate(data);
       },
 
       (_data: any) => {}
@@ -52,15 +54,48 @@ export class CombustionThermometer extends TemperatureDevice {
   }
 
   private parseStatusUpdate(temperatureRawStatus: Uint8Array) {
+    const lograngeArray = new Uint32Array(
+      temperatureRawStatus.slice(0, 8).buffer
+    );
+
+    /**
+     * Parsing
+     */
+    const logrange = {
+      min: lograngeArray[0],
+      max: lograngeArray[1],
+    };
+
+    const temperatures = this.readTemperatures(
+      temperatureRawStatus.slice(8, 21)
+    );
+
+    const modeByte = temperatureRawStatus[21];
+
+    const modeAndID = {
+      mode: (modeByte >> 0) & 0b11,
+      color: (modeByte >> 2) & 0b111,
+      id: (modeByte >> 5) & 0b111,
+    };
+
+    const battery = temperatureRawStatus[22] & 0b01;
+    const virtualSensors = {
+      coreIndex: (temperatureRawStatus[22] >> 1) & 0b111,
+      surfaceIndex: (temperatureRawStatus[22] >> 4) & 0b11,
+      ambientIndex: (temperatureRawStatus[22] >> 6) & 0b11,
+      core: undefined,
+      surface: undefined,
+      ambient: undefined,
+    };
+    virtualSensors.core = temperatures[virtualSensors.coreIndex];
+    virtualSensors.surface = temperatures[virtualSensors.surfaceIndex];
+    virtualSensors.ambient = temperatures[virtualSensors.ambientIndex];
+
     this.logger.log(
       'temperatureRawStatus received is: ' + temperatureRawStatus
     );
 
-    const tipTemperature =
-      ((temperatureRawStatus[0] & 0xff) << 5) |
-      ((temperatureRawStatus[1] & 0xf8) >> 3);
-    console.log(tipTemperature);
-    this.setTemperature(tipTemperature, temperatureRawStatus);
+    this.setTemperature(virtualSensors.core, temperatureRawStatus);
   }
 
   private deattachNotification() {
@@ -71,5 +106,39 @@ export class CombustionThermometer extends TemperatureDevice {
       (e: any) => {},
       (e: any) => {}
     );
+  }
+
+  public convertToTemperature(raw) {
+    const convert = raw * 0.05 - 20;
+    // Round to 0.05
+    const rounded = Math.round(convert * 200) / 200;
+    return rounded;
+  }
+
+  public readTemperatures(uint8Array) {
+    let result = [];
+    let buffer = 0;
+    let bufferLength = 0;
+
+    // Push a byte to the buffer, see if we have a full temperature, if so remove it from the end of the buffer leaving some bits for the next byte
+    for (let i = 0; i < uint8Array.length; i++) {
+      // The new 8 bits are going in the front, our previous bit go in the end of the new value
+      buffer = (uint8Array[i] << bufferLength) | buffer;
+      // We consumed 1 byte / 8 bit
+      bufferLength += 8;
+
+      // Do we have a full temperature value?
+      if (bufferLength >= this.bitsPerTemperature) {
+        // consume the last 13 bits and store in the temperature array
+        const value = (buffer &= (1 << this.bitsPerTemperature) - 1);
+        result.push(this.convertToTemperature(value));
+
+        // remove the last 13 bit
+        buffer = buffer >> this.bitsPerTemperature;
+        bufferLength -= this.bitsPerTemperature;
+      }
+    }
+
+    return result;
   }
 }
