651755c5604ab1570c941d95f86e1b47f6dc7a6a
randomcoffeesnob <ydietler@gmail.com>
2024-09-24 21:15:28 +0200

    Timemore Scale integration

--- src/services/coffeeBluetoothDevices/coffee-bluetooth-devices.service.ts
+++ src/services/coffeeBluetoothDevices/coffee-bluetooth-devices.service.ts
@@ -43,6 +43,7 @@ import { BasicGrillThermometer } from 'src/classes/devices/basicGrillThermometer
 import { MeaterThermometer } from 'src/classes/devices/meaterThermometer';
 import { CombustionThermometer } from '../../classes/devices/combustionThermometer';
 import { ArgosThermometer } from '../../classes/devices/argosThermometer';
+import { TimemoreScale } from 'src/classes/devices/timemoreScale';
 
 declare var device: any;
 declare var ble: any;
@@ -715,6 +716,13 @@ export class CoffeeBluetoothDevicesService {
       return {
         id: deviceScale.id,
         type: ScaleType.BOKOOSCALE,
+      };
+    }
+    if (TimemoreScale.test(deviceScale)) {
+      this.logger.log('BleManager - We found a Timemore scale');
+      return {
+        id: deviceScale.id,
+        type: ScaleType.TIMEMORESCALE,
       };
     }
 
