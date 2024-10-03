a65653b0dbf054e27135a162c4f777e5deecf436
Marius Renner <marius@mariusrenner.de>
2024-10-03 11:46:04 +0200

    Dynamically change web view origin per platform
    To retain all data in the web view IndexedDB when updating from Cordova
    builds, it is necessary to retain the same origin for the app files the
    Cordova version used.
    
    Both the scheme and the hostname are different between iOS and Android,
    but Capacitor only support changing the scheme natively per platform. As
    a workaround, the configuration now dynamically parses its own command
    line arguments to determine the platform that is currently being
    configured. Until Capacitor exposes the current platform in a documented
    way (see https://github.com/ionic-team/capacitor/issues/3976), this will
    have to suffice.
    
    This slightly changes the workflow. Now, it is not possible to run
    "npx cap sync" anymore to sync all platforms. Instead, the platforms
    must be synced separately, for example using "npx cap sync android" and
    "npx cap sync ios". For convenience, the command "npm run capsync" is
    also provided to sync all platforms.
    
    Note that the project currently only contains an Android target. The
    ios target must be added to the "npm run capsync" command once it is
    part of the repository.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,38 +1,91 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'com.beanconqueror.app',
-  appName: 'Beanconqueror',
-  webDir: 'www',
-  server: {
-    // Using this hostname and scheme is required to retain access
-    // to the __baristaDB on Android when updating from Cordova builds
-    hostname: 'beanconqueror.com',
-    androidScheme: 'https',
-    // TODO Capacitor migration: Find out the correct settings to retain data on iOS
-  },
-  cordova: {
-    preferences: {
-      ScrollEnabled: 'false',
-      'android-minSdkVersion': '24',
-      'android-targetSdkVersion': '34',
-      'android-compileSdkVersion': '34',
-      'android-buildToolsVersion': '34.0.0',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '300',
-      StatusBarOverlaysWebView: 'false',
-      StatusBarBackgroundColor: '#F0F0F0',
-      StatusBarStyle: 'light',
-      AndroidPersistentFileLocation: 'Compatibility',
-      AndroidExtraFilesystems:
-        'files,files-external,documents,cache,cache-external',
-      iosExtraFilesystems:
-        'library,library-nosync,documents,documents-nosync,cache,bundle,root',
-    },
-  },
+type Platform = 'android' | 'ios';
+
+const getPlatform: () => Platform = () => {
+  /* This is slightly hacky, but it is build-time configuration and we can
+   * just fail if something unexpected happens.
+   * See https://github.com/ionic-team/capacitor/discussions/4173
+   *
+   * https://github.com/ionic-team/capacitor/issues/3976 would be a much better
+   * solution if it is ever implemented, but for now this has to suffice.
+   */
+  const platform = process.argv[3];
+  switch (platform) {
+    case 'android':
+      return 'android';
+    case 'ios':
+      return 'ios';
+    case undefined:
+      const errorMessage =
+        '==================================================================\n' +
+        'Cannot determine platform from arguments.\n' +
+        'Please run platform configuration seperately, for example ' +
+        '"npx cap sync android" and "npx cap sync ios" ' +
+        'instead of just "npx cap sync".\n' +
+        'If you just want to sync all platforms, run "npm run capsync" as a ' +
+        'convenient shorcut.\n' +
+        '==================================================================\n';
+      console.error(errorMessage);
+      throw new Error('\n' + errorMessage);
+    default:
+      throw new Error(`Unexpected platform argument: ${platform}`);
+  }
 };
+
+const createConfig = () => {
+  const config: CapacitorConfig = {
+    appId: 'com.beanconqueror.app',
+    appName: 'Beanconqueror',
+    webDir: 'www',
+    server: {
+      // Will be set in platform section below
+    },
+    ios: {
+      // Will be set in platform section below
+    },
+    cordova: {
+      preferences: {
+        ScrollEnabled: 'false',
+        'android-minSdkVersion': '24',
+        'android-targetSdkVersion': '34',
+        'android-compileSdkVersion': '34',
+        'android-buildToolsVersion': '34.0.0',
+        SplashMaintainAspectRatio: 'true',
+        FadeSplashScreenDuration: '300',
+        SplashShowOnlyFirstTime: 'false',
+        SplashScreen: 'screen',
+        SplashScreenDelay: '300',
+        StatusBarOverlaysWebView: 'false',
+        StatusBarBackgroundColor: '#F0F0F0',
+        StatusBarStyle: 'light',
+        AndroidPersistentFileLocation: 'Compatibility',
+        AndroidExtraFilesystems:
+          'files,files-external,documents,cache,cache-external',
+        iosExtraFilesystems:
+          'library,library-nosync,documents,documents-nosync,cache,bundle,root',
+      },
+    },
+  };
+
+  switch (getPlatform()) {
+    case 'android':
+      // Using this hostname and scheme is required to retain access
+      // to the __baristaDB on Android when updating from Cordova builds
+      config.server.hostname = 'beanconqueror.com';
+      config.server.androidScheme = 'https';
+      break;
+    case 'ios':
+      // Using this hostname and scheme is required to retain access
+      // to the __baristaDB on iOS when updating from Cordova builds
+      config.server.hostname = 'localhost';
+      config.ios.scheme = 'ionic';
+      break;
+  }
+
+  return config;
+};
+
+const config: CapacitorConfig = createConfig();
 
 export default config;
