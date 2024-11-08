404b40172cd6b990b0e04244831ed0c87a57834a
Lars Saalbach <lars.saalbach@googlemail.com>
2024-11-08 19:05:49 +0100

    Changing toFixed to a pipe to dodge ngZone detection

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { KeysPipe } from './keys';
 import { EnumToArrayPipe } from './enumToArray';
 import { FormatDatePipe } from './formatDate';
+import { ToFixedPipe } from './toFixed';
 
 @NgModule({
-  declarations: [EnumToArrayPipe, FormatDatePipe, KeysPipe],
-  exports: [EnumToArrayPipe, FormatDatePipe, KeysPipe],
+  declarations: [EnumToArrayPipe, FormatDatePipe, KeysPipe, ToFixedPipe],
+  exports: [EnumToArrayPipe, FormatDatePipe, KeysPipe, ToFixedPipe],
 })
 export class PipesModule {}
---
+++ src/pipes/toFixed.ts
@@ -0,0 +1,30 @@
+/** Core */
+import { Pipe, PipeTransform } from '@angular/core';
+
+/** Third party */
+
+@Pipe({ name: 'toFixed' })
+export class ToFixedPipe implements PipeTransform {
+  public transform(value, format, args?: Array<string>): any {
+    // SAMPLE - http://jsfiddle.net/kunycrkb/
+    try {
+      if (value === null || value === undefined || value === '') {
+        return 0;
+      }
+      const parsedFloat = parseFloat(value);
+      if (isNaN(parsedFloat)) {
+        return 0;
+      }
+
+      let dp = 2;
+      if (format === null || format === undefined || format === '') {
+      } else {
+        dp = format;
+      }
+
+      return +parsedFloat.toFixed(dp);
+    } catch (ex) {
+      return 0;
+    }
+  }
+}
