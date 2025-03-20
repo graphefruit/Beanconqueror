3de1ac4cfc2c4232a2badc92e0d5e63069937557
AV-IO <ianguile513@gmail.com>
2025-03-19 23:11:28 -0700

    Storing refractometer data in variables

--- src/classes/devices/difluidR2Refractometer.ts
+++ src/classes/devices/difluidR2Refractometer.ts
@@ -9,6 +9,14 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
   public static CHAR_UUID = 'AA01';
   public static DEVICE_NAME = 'r2';
 
+  public MODEL_NAME = '';
+  public FIRMWARE_VERSION = '';
+  public SERIAL_NUMBER = '               ';
+
+  public TEMPERATURE_UNIT = 'C';
+  public SCREEN_BRIGHTNESS = 0;
+  public TEST_COUNT = 0;
+
   private logger: Logger;
 
   constructor(data: PeripheralData) {
@@ -32,7 +40,9 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
       await this.setDeviceAutoNotification(diFluid.R2.settings.autoTest.Off);
     }, 100);
     await setTimeout(async () => {
-      await this.setDeviceTemperatureUnit(diFluid.R2.settings.temperatureUnit.C);
+      await this.setDeviceTemperatureUnit(
+        diFluid.R2.settings.temperatureUnit.C,
+      );
     }, 100);
   }
 
@@ -47,40 +57,39 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
   }
 
   public override async requestRead() {
+    this.logger.log('requesting TDS reading');
     await this.write(
-      diFluid.buildRawCmd(
-        {
-          func: diFluid.func.DEVICE_ACTION,
-          cmd: diFluid.R2.action.SINGLE_TEST,
-          data: new Uint8Array([])
-        }
-      )
+      diFluid.buildRawCmd({
+        func: diFluid.func.DEVICE_ACTION,
+        cmd: diFluid.R2.action.SINGLE_TEST,
+        data: new Uint8Array([]),
+      }),
     );
   }
 
-  public async setDeviceAutoNotification(notifications: diFluid.R2.settings.autoTest) {
+  public async setDeviceAutoNotification(
+    notifications: diFluid.R2.settings.autoTest,
+  ) {
     this.logger.log('enabling auto notifications');
     await this.write(
-      diFluid.buildRawCmd(
-        {
-          func: diFluid.func.DEVICE_SETTINGS,
-          cmd: diFluid.R2.settings.AUTO_TEST_STATUS,
-          data: new Uint8Array([notifications])
-        }
-      )
+      diFluid.buildRawCmd({
+        func: diFluid.func.DEVICE_SETTINGS,
+        cmd: diFluid.R2.settings.AUTO_TEST_STATUS,
+        data: new Uint8Array([notifications]),
+      }),
     );
   }
 
-  public async setDeviceTemperatureUnit(unit: diFluid.R2.settings.temperatureUnit) {
+  public async setDeviceTemperatureUnit(
+    unit: diFluid.R2.settings.temperatureUnit,
+  ) {
     this.logger.log('set R2 to celsius');
     await this.write(
-      diFluid.buildRawCmd(
-        {
-          func: diFluid.func.DEVICE_SETTINGS,
-          cmd: diFluid.R2.settings.TEMPERATURE_UNIT,
-          data: new Uint8Array([unit])
-        }
-      )
+      diFluid.buildRawCmd({
+        func: diFluid.func.DEVICE_SETTINGS,
+        cmd: diFluid.R2.settings.TEMPERATURE_UNIT,
+        data: new Uint8Array([unit]),
+      }),
     );
   }
 
@@ -96,7 +105,7 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
         },
         (e: any) => {
           resolve(false);
-        }
+        },
       );
     });
   }
@@ -109,13 +118,13 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
       async (_data: any) => {
         this.parseStatusUpdate(new Uint8Array(_data));
       },
-      (_data: any) => {}
+      (_data: any) => {},
     );
   }
 
   /**
    * @param rawStatus - Uint8Array bytes from device
-   * 
+   *
    * @see {@link https://github.com/DiFluid/difluid-sdk-demo/blob/master/docs/protocolR2.md | Protocol Documentation}
    */
   private async parseStatusUpdate(rawStatus: Uint8Array) {
@@ -123,43 +132,56 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
     try {
       status = diFluid.parseRawStatus(rawStatus);
     } catch (err) {
-      this.logger.error("Could not parse raw status message. Err: " + err.message);
+      this.logger.error(
+        'Could not parse raw status message. Err: ' + err.message,
+      );
       return;
     }
 
     if (status.func === diFluid.func.DEVICE_INFO) {
       if (status.cmd === diFluid.R2.info.SERIAL_NUMBER) {
+        const part = status.data[0];
+        this.SERIAL_NUMBER =
+          this.SERIAL_NUMBER.substring(0, part * 5) +
+          diFluid.parseString(status.data.slice(1)) +
+          this.SERIAL_NUMBER.substring(part * 5 + 5);
         this.logger.log(
-          "Serial Number (" + (status.data[0] + 1).toString() + "/3): "
-          + diFluid.parseString(status.data.slice(1))
+          'Serial Number (' + (status.data[0] + 1).toString() + '/3)',
         );
       } else if (status.cmd === diFluid.R2.info.DEVICE_MODEL) {
-        this.logger.log("Device Model: " + diFluid.parseString(status.data));
+        this.MODEL_NAME = diFluid.parseString(status.data);
+        this.logger.log('Device Model: ' + this.MODEL_NAME);
       } else if (status.cmd === diFluid.R2.info.FIRMWARE_VERSION) {
-        this.logger.log("Firmware version: " + diFluid.parseString(status.data));
+        this.FIRMWARE_VERSION = diFluid.parseString(status.data);
+        this.logger.log('Firmware version: ' + this.FIRMWARE_VERSION);
       }
     } else if (status.func === diFluid.func.DEVICE_SETTINGS) {
       if (status.cmd === diFluid.R2.settings.TEMPERATURE_UNIT) {
         if (status.data[0] === diFluid.R2.settings.temperatureUnit.C) {
-          this.logger.log("Temperature Unit: C");
+          this.TEMPERATURE_UNIT = 'C';
         } else if (status.data[0] === diFluid.R2.settings.temperatureUnit.F) {
-          this.logger.log("Temperature Unit: F");
+          this.TEMPERATURE_UNIT = 'F';
         }
+        this.logger.log('Temperature Unit: ' + this.TEMPERATURE_UNIT);
       } else if (status.cmd === diFluid.R2.settings.AUTO_TEST_STATUS) {
         if (status.data[0] === diFluid.R2.settings.autoTest.Off) {
-          this.logger.log("Auto Test: Off");
+          this.logger.log('Auto Test: Off');
         } else if (status.data[0] === diFluid.R2.settings.autoTest.On) {
-          this.logger.log("Auto Test: On");
+          this.logger.log('Auto Test: On');
         }
       } else if (status.cmd === diFluid.R2.settings.SCREEN_BRIGHTNESS) {
-        this.logger.log("Screen Brightness: " + status.data[0].toString() + "%");
+        this.SCREEN_BRIGHTNESS = status.data[0];
+        this.logger.log(
+          'Screen Brightness: ' + this.SCREEN_BRIGHTNESS.toString() + '%',
+        );
       } else if (status.cmd === diFluid.R2.settings.NUMBER_OF_TESTS) {
-        this.logger.log("Number of Tests: " + status.data[0].toString());
+        this.TEST_COUNT = status.data[0];
+        this.logger.log('Number of Tests: ' + this.TEST_COUNT.toString());
       }
     } else if (status.func === diFluid.func.DEVICE_ACTION) {
       if (
-        status.cmd === diFluid.R2.action.SINGLE_TEST
-        || status.cmd === diFluid.R2.action.AVERAGE_TEST
+        status.cmd === diFluid.R2.action.SINGLE_TEST ||
+        status.cmd === diFluid.R2.action.AVERAGE_TEST
       ) {
         if (status.data[0] === diFluid.R2.action.test.TEST_STATUS) {
           switch (status.data[1]) {
@@ -185,37 +207,48 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
               this.logger.log('Loop Test Finished');
               break;
             case diFluid.R2.action.test.status.AVERAGE_TEST_ONGOING_INVALID:
-              this.logger.log('Average Test Ongoing, but taking longer than expected');
+              this.logger.log(
+                'Average Test Ongoing, but taking longer than expected',
+              );
               break;
             case diFluid.R2.action.test.status.TEST_START:
               this.logger.log('Test Started');
               break;
           }
         } else if (
-          status.data[0] === diFluid.R2.action.test.TEMPERATURE_INFO
-          || status.data[0] === diFluid.R2.action.test.AVERAGE_TEMPERATURE_INFO
+          status.data[0] === diFluid.R2.action.test.TEMPERATURE_INFO ||
+          status.data[0] === diFluid.R2.action.test.AVERAGE_TEMPERATURE_INFO
         ) {
-          if (status.data[0] === diFluid.R2.action.test.AVERAGE_TEMPERATURE_INFO) {
+          if (
+            status.data[0] === diFluid.R2.action.test.AVERAGE_TEMPERATURE_INFO
+          ) {
             this.logger.log('Average Temperature Received');
             this.logger.log(
               'Recieved Average Test (' +
-              status.data[5].toString() + '/' +
-              status.data[6].toString() + ')'
-            )
+                status.data[5].toString() +
+                '/' +
+                status.data[6].toString() +
+                ')',
+            );
           } else {
             this.logger.log('Temperature Received');
-            // ToDo: use temperature unit `data[5]` to ensure data integrity
+            if (status.data[5] === diFluid.R2.settings.temperatureUnit.C) {
+              this.TEMPERATURE_UNIT = 'C';
+            } else if (
+              status.data[5] === diFluid.R2.settings.temperatureUnit.F
+            ) {
+              this.TEMPERATURE_UNIT = 'F';
+            }
           }
           // Setting based on average of prism and tank, adjusted to remove 10* multiplier
           this.setTempReading(
-            (
-              this.getInt(status.data.slice(1, 3)) +
-              this.getInt(status.data.slice(3, 5))
-            ) / 20
+            (this.getInt(status.data.slice(1, 3)) +
+              this.getInt(status.data.slice(3, 5))) /
+              20,
           );
         } else if (
-          status.data[0] === diFluid.R2.action.test.TEST_RESULT
-          || status.data[0] === diFluid.R2.action.test.AVERAGE_RESULT
+          status.data[0] === diFluid.R2.action.test.TEST_RESULT ||
+          status.data[0] === diFluid.R2.action.test.AVERAGE_RESULT
         ) {
           if (status.data[0] === diFluid.R2.action.test.AVERAGE_RESULT) {
             this.logger.log('Average TDS Received');
@@ -223,9 +256,7 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
             this.logger.log('TDS Received');
           }
 
-          this.setTdsReading(
-            this.getInt(status.data.slice(1, 3)) / 100
-          );
+          this.setTdsReading(this.getInt(status.data.slice(1, 3)) / 100);
           this.resultEvent.emit(null);
         }
       } else if (status.cmd === diFluid.R2.action.CALIBRATION_RESULT) {
@@ -244,11 +275,17 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
         if (rawStatus[6] === diFluid.R2.action.errorClass.GENERAL) {
           if (rawStatus[7] === diFluid.R2.action.error.general.TEST_ERROR) {
             this.logger.log('Test Error');
-          } else if (rawStatus[7] === diFluid.R2.action.error.general.CALIBRATION_FAILED) {
+          } else if (
+            rawStatus[7] === diFluid.R2.action.error.general.CALIBRATION_FAILED
+          ) {
             this.logger.log('Calibration Failed');
-          } else if (rawStatus[7] === diFluid.R2.action.error.general.NO_LIQUID) {
+          } else if (
+            rawStatus[7] === diFluid.R2.action.error.general.NO_LIQUID
+          ) {
             this.logger.log('No Liquid');
-          } else if (rawStatus[7] === diFluid.R2.action.error.general.BEYOND_RANGE) {
+          } else if (
+            rawStatus[7] === diFluid.R2.action.error.general.BEYOND_RANGE
+          ) {
             this.logger.log('Beyond Range');
           }
         } else if (rawStatus[6] === diFluid.R2.action.errorClass.HARDWARE) {
@@ -263,14 +300,14 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
   /**
    * @param buffer - A Uint8Array to be parsed as a single integer
    * supports up to a 64bit int
-   * 
+   *
    * @returns a `number`
    */
   public getInt(buffer: Uint8Array): number {
     let sum = 0;
     buffer = buffer.reverse();
     for (let idx = 0; idx < buffer.length && idx <= 4; idx++) {
-      sum += (buffer[idx] << (8 * idx));
+      sum += buffer[idx] << (8 * idx);
     }
 
     return sum;
@@ -282,7 +319,7 @@ export class DiFluidR2Refractometer extends RefractometerDevice {
       DiFluidR2Refractometer.SERVICE_UUID,
       DiFluidR2Refractometer.CHAR_UUID,
       (e: any) => {},
-      (e: any) => {}
+      (e: any) => {},
     );
   }
 }
