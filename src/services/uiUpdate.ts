60b9e3760d6a76653c253ec47df7ada274c381f1
Lars Saalbach <lars.saalbach@googlemail.com>
2025-03-24 21:41:32 +0100

    updating labels for 8.2.0

--- src/services/uiUpdate.ts
+++ src/services/uiUpdate.ts
@@ -635,7 +635,7 @@ export class UIUpdate {
         versionCode = (await App.getInfo()).version;
       } else {
         // Hardcored for testing
-        versionCode = '8.1.0';
+        versionCode = '8.2.0';
       }
       const version: Version = this.uiVersionStorage.getVersion();
       const displayingVersions =
