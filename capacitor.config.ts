29b53259596ce169f484c9fdbe3132dd293220d3
Lars Saalbach <lars.saalbach@googlemail.com>
2025-08-07 22:44:04 +0200

    #460 - Making android statusbar not overlaying

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -86,6 +86,9 @@ const createConfig = () => {
         androidScaleType: 'CENTER_CROP',
         useDialog: false, // required to set the correct scale type
       },
+      StatusBar: {
+        overlaysWebView: false,
+      },
     },
   };
 
