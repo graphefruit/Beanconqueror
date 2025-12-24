ae7fa50239c78829938e14a1805dedf4c6f06a1b
Marius Renner <marius@mariusrenner.de>
2025-12-24 17:10:17 +0100

    Update to Angular 21
    This updates all Angular dependencies to version 21. Mandatory
    migrations were also performed, but optional migrations were not. The
    optional migrations will be performed later.
    
    This also configures .browserslistrc to match Angular's support to
    prevent build warnings.
    
    A workaround for an upstream ionic issue (see
    https://github.com/ionic-team/ionic-framework/issues/30650) is also
    added in tsconfig.json to prevent build errors.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    applicationProviders: [provideZoneChangeDetection()],
+  })
+  .catch((err) => console.log(err));
