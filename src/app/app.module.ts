0b3b5f343a0f438e4668d95dd27461dea42afd51
Marius Renner <marius@mariusrenner.de>
2026-01-06 16:19:05 +0100

    Extract custom Angular error handler into separate file
    Declaring the custom error handler class in the module file itself
    confuses the Angular standalone migration which will be performed at a
    later time.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,61 +18,7 @@ import {
 } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
-import { UILog } from '../services/uiLog';
-
-class MyErrorHandler implements ErrorHandler {
-  private ERROR_ORIGINAL_ERROR = 'ngOriginalError';
-  private _console;
-  constructor() {
-    this._console = console;
-  }
-  public handleError(error) {
-    // do something with the exception
-    const originalError = this._findOriginalError(error);
-    this._console.error('ERROR', error);
-    if (originalError) {
-      this._console.error('ORIGINAL ERROR', originalError);
-    }
-    try {
-      try {
-        UILog.getInstance().unhandledError(JSON.stringify(error));
-      } catch (ex) {
-        try {
-          UILog.getInstance().unhandledError(error.toString());
-        } catch (ex) {}
-      }
-      if (originalError) {
-        try {
-          UILog.getInstance().unhandledError(JSON.stringify(originalError));
-        } catch (ex) {
-          try {
-            UILog.getInstance().unhandledError(originalError.toString());
-          } catch (ex) {}
-        }
-      }
-    } catch (ex) {}
-  }
-
-  private wrappedError(message, originalError) {
-    const msg = `${message} caused by: ${
-      originalError instanceof Error ? originalError.message : originalError
-    }`;
-    const error = Error(msg);
-    error[this.ERROR_ORIGINAL_ERROR] = originalError;
-    return error;
-  }
-  private getOriginalError(error) {
-    return error[this.ERROR_ORIGINAL_ERROR];
-  }
-  /** @internal */
-  private _findOriginalError(error) {
-    let e = error && this.getOriginalError(error);
-    while (e && this.getOriginalError(e)) {
-      e = this.getOriginalError(e);
-    }
-    return e || null;
-  }
-}
+import { BeanconquerorErrorHandler } from '../classes/angular/BeanconquerorErrorHandler';
 
 @NgModule({
   declarations: [AppComponent],
@@ -107,7 +53,7 @@ class MyErrorHandler implements ErrorHandler {
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: ErrorHandler, useClass: MyErrorHandler },
+    { provide: ErrorHandler, useClass: BeanconquerorErrorHandler },
     provideHttpClient(withInterceptorsFromDi()),
   ],
 })
