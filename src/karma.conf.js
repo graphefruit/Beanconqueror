3e081c2ef743b9d2f1a0fea4438f608c38676894
luuciarr06 <lucricrui@alum.us.es>
2026-02-28 16:32:07 +0100

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,9 +1,11 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
-    basePath: '../',
+    basePath: '', // Cambiado a vacío para que use la raíz relativa a donde esté el archivo
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
@@ -15,22 +17,28 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
+      // Forzamos la salida a la carpeta 'coverage' en la raíz del proyecto
       dir: require('path').join(__dirname, '../coverage'),
+      // lcovonly es el formato que exige SonarCloud para importar cobertura
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml','coverage-istanbul'],
+    // Añadimos 'coverage-istanbul' a los reporters para activar la generación
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    // En CI, autoWatch debe ser false para que no se quede esperando cambios
+    autoWatch: false,
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     },
-    singleRun: false,
+    // singleRun debe ser true para que el proceso de GitHub Actions finalice tras los tests
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
