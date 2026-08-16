1a1ec93985eebe53e2a571695eed4a6d2b13cf90
Michalis Martinis <mixamart98@gmail.com>
2026-08-16 11:50:51 +0300

    feat: add OXO Rapid Brewer preparation

--- src/classes/preparation/preparation.ts
+++ src/classes/preparation/preparation.ts
@@ -173,6 +173,8 @@ export class Preparation implements IPreparation {
         return PREPARATION_STYLE_TYPE.ESPRESSO;
       case PREPARATION_TYPES.MOVE2:
         return PREPARATION_STYLE_TYPE.ESPRESSO;
+      case PREPARATION_TYPES.OXO_RAPID_BREWER:
+        return PREPARATION_STYLE_TYPE.PERCOLATION;
       default:
         return PREPARATION_STYLE_TYPE.POUR_OVER;
     }
@@ -267,6 +269,8 @@ export class Preparation implements IPreparation {
         return 'beanconqueror-preparation-gaggiuino';
       case PREPARATION_TYPES.MOVE2:
         return 'beanconqueror-preparation-move2';
+      case PREPARATION_TYPES.OXO_RAPID_BREWER:
+        return 'beanconqueror-preparation-oxo-rapid-brewer';
       default:
         return 'beanconqueror-preparation-custom';
     }
